df7f119e41378254aca995486c5acd62119c8e33
amiltonwong <amiltonwong@gmail.com>
2017-12-03 03:26:08 +0800

    commit

---
+++ hw1/build/CMakeFiles/sayhello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sayhello.dir/src/useHello.cpp.o"
+  "sayhello.pdb"
+  "sayhello"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sayhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
