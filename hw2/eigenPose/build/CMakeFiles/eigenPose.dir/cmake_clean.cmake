c285f849c29f8f4dd6118cc51c084385e0c0643a
amiltonwong <amiltonwong@gmail.com>
2017-12-14 09:54:58 -0500

    commit

---
+++ hw2/eigenPose/build/CMakeFiles/eigenPose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eigenPose.dir/eigenPose.cpp.o"
+  "eigenPose.pdb"
+  "eigenPose"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eigenPose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
