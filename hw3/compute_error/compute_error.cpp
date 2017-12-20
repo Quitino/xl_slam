eeac5ebd503bd041d321f14b52d550f8c5fc4b46
amiltonwong <amiltonwong@gmail.com>
2017-12-20 11:24:19 -0500

    commit

--- hw3/compute_error/compute_error.cpp
+++ hw3/compute_error/compute_error.cpp
@@ -16,6 +16,8 @@ using namespace std;
 // function for plotting trajectory, don't edit this code
 // start point is red and end point is blue
 void DrawTrajectory(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>>);
+void DrawCompare(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses_e, 
+		 vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses_g);
 
 int main(int argc, char **argv) {
 
@@ -34,7 +36,7 @@ int main(int argc, char **argv) {
 
     ifstream T_gt("./groundtruth.txt");
     if (!T_gt) {
-      cout<<"unable to read groundtruth.txt file!"<<endl;
+      cout<<"unable to read groundtruth.2txt file!"<<endl;
 	exit(1);
     }
 
@@ -67,13 +69,9 @@ int main(int argc, char **argv) {
       Eigen::Matrix<double, 6, 1> err;
       err = (g.inverse()*e).log();
       a.push_back(err.norm());
-      //a << err.norm();
     }
    
-   // compute RMSE for err
-   //cout<<a<<endl;
-   
-   
+  
     // compute RMSE for err
     for (auto i=0; i<a.size(); i++) {
       result += a[i]*a[i];
@@ -82,12 +80,65 @@ int main(int argc, char **argv) {
     cout<<"RMSE is: "<<result<<endl;
     // end your code here
 
-    // draw trajectory in pangolin
-    DrawTrajectory(poses_e);
-    
-    DrawTrajectory(poses_g);
+    // draw gt and est. trajectory in pangolin   
+    DrawCompare(poses_e, poses_g);
     return 0;
 }
+
+
+void DrawCompare(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses_e, 
+		 vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses_g) {
+    if (poses_e.empty() || poses_g.empty()) {
+        cerr << "Trajectory is empty!" << endl;
+        return;
+    }
+
+    // create pangolin window and plot the trajectory
+    pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+            pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+            pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+            .SetHandler(new pangolin::Handler3D(s_cam));
+
+
+    while (pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glLineWidth(2);
+        for (size_t i = 0; i < poses_e.size() - 1; i++) {
+            glColor3f(1 - (float) i / poses_e.size(), 0.0f, (float) i / poses_e.size());
+            glBegin(GL_LINES);
+            auto p1 = poses_e[i], p2 = poses_e[i + 1];
+	    auto p3 = poses_g[i], p4 = poses_g[i + 1];
+            glVertex3d(p1.translation()[0], p1.translation()[1], p1.translation()[2]);
+            glVertex3d(p2.translation()[0], p2.translation()[1], p2.translation()[2]);
+	    glEnd();
+	    
+            glColor3f(0.0f , 1.0f, 0.0f);
+            glBegin(GL_LINES);	    
+	    glVertex3d(p3.translation()[0], p3.translation()[1], p3.translation()[2]);
+            glVertex3d(p4.translation()[0], p4.translation()[1], p4.translation()[2]);
+            glEnd();
+        }
+        
+        pangolin::FinishFrame();
+        usleep(5000);   // sleep 5 ms
+    }
+
+}
+
+
 
 /*******************************************************************************************/
 void DrawTrajectory(vector<Sophus::SE3, Eigen::aligned_allocator<Sophus::SE3>> poses) {
