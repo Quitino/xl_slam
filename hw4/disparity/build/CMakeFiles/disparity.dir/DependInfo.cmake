78ffdb59cf2cefd43af23269e49cb2c9fd87305e
Milton Wong <amiltonwong@gmail.com>
2018-01-26 19:38:01 +0800

    commit

---
+++ hw4/disparity/build/CMakeFiles/disparity.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/code/xl_slam/hw4/disparity/disparity.cpp" "/data/code/xl_slam/hw4/disparity/build/CMakeFiles/disparity.dir/disparity.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/data/lib/opencv-3.0.0/release/include"
+  "/usr/include/eigen3"
+  "/usr/include/vtk-5.8"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/include/eigen3"
+  "/usr/include/ni"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
