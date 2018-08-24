14015eef63e7fce180b7586a3bc12dafcc1501fe
root <amiltonwong@gmail.com>
2018-08-25 01:00:35 +0800

    commit

---
+++ hw5/icp_align/build/CMakeFiles/icp_align.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icp_align.dir/icp_align.cpp.o"
+  "icp_align.pdb"
+  "icp_align"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icp_align.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
